b0992ab319ed121a963e1eb6132377aca75a2e9e
Harpreet Singh <harry.singh1064@gmail.com>
2016-10-16 01:33:00 +0530

    Created GOEuroNetworkCommunicationController for rest api communication.

---
+++ GoEuro/GOEuroNetworkCommunicationController.h
@@ -0,0 +1,13 @@
+//
+//  GOEuroNetworkCommunicationController.h
+//  GoEuro
+//
+//  Created by Quinto Technologies Pvt. Ltd. on 16/10/16.
+//  Copyright © 2016 Harpreet Singh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GOEuroNetworkCommunicationController : NSObject
+
+@end
---
+++ GoEuro/GOEuroNetworkCommunicationController.m
@@ -0,0 +1,13 @@
+//
+//  GOEuroNetworkCommunicationController.m
+//  GoEuro
+//
+//  Created by Quinto Technologies Pvt. Ltd. on 16/10/16.
+//  Copyright © 2016 Harpreet Singh. All rights reserved.
+//
+
+#import "GOEuroNetworkCommunicationController.h"
+
+@implementation GOEuroNetworkCommunicationController
+
+@end
