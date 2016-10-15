2c295f43e5a50ad60035ed3e634ed9b8b74ae6c4
Harpreet Singh <harry.singh1064@gmail.com>
2016-10-16 00:17:09 +0530

    Initial Commit

---
+++ GoEuro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoEuro
+//
+//  Created by Quinto Technologies Pvt. Ltd. on 16/10/16.
+//  Copyright © 2016 Harpreet Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoEuro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoEuro
+//
+//  Created by Quinto Technologies Pvt. Ltd. on 16/10/16.
+//  Copyright © 2016 Harpreet Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
