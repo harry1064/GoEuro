6cbd9456d89adcd9c18d0fb82fc65fd59e7d2a84
Harpreet Singh <harry.singh1064@gmail.com>
2016-10-16 04:58:47 +0530

    Loading cache date when user offline i.e not connected to network
    As soon as user get connected to network, data is refreshed from server.

--- GoEuro/GOEuroNetworkCommunicationController.m
+++ GoEuro/GOEuroNetworkCommunicationController.m
@@ -7,12 +7,18 @@
 //
 
 #import "GOEuroNetworkCommunicationController.h"
-
+#import <AFNetworkReachabilityManager.h>
 @implementation GOEuroNetworkCommunicationController
 
 + (void) makeGetRequestToUrl:(NSString *)urlString
          withCompletionBlock:(networkCompletionBlock) completionBlock {
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    if([[AFNetworkReachabilityManager sharedManager] isReachable] || [[AFNetworkReachabilityManager sharedManager]  isReachableViaWiFi]) {
+        [manager.requestSerializer setCachePolicy:NSURLRequestReloadIgnoringCacheData];
+    } else {
+         [manager.requestSerializer setCachePolicy:NSURLRequestReturnCacheDataElseLoad];
+    }
+   
     [manager GET:urlString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
         completionBlock(true, responseObject);
     } failure:^(NSURLSessionTask *operation, NSError *error) {
